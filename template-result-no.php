c3b7cc50dd79d8c434f03355df3e2a78df82d63e
Benjamin Intal <bfintal@gmail.com>
2015-04-24 20:19:59 +0800

    Changed copy

--- template-result-no.php
+++ template-result-no.php
@@ -4,6 +4,9 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 <script type="text/html" id="tmpl-result-no">
 	
-	<span>Not yet filed in LTO</span>
+	<div class="result-no">
+		<div>Not yet filed in LTO</div>
+		<div class="disclaimer">* For new filings, it may take up to 3-4 days for your filing to reflect in our database.</div>
+	</div>
 
 </script>
--- template-result-ok.php
+++ template-result-ok.php
@@ -4,7 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 <script type="text/html" id="tmpl-result-ok">
 	
-	<span class="date">{{ data.date }}</span>
-	<span class="unit">{{ data.unit }}</span>
+	<div class="result-ok">
+		<div class="date">Registration filing received on {{ data.date }}</div>
+		<div class="unit">Vehicle: {{ data.unit }}</div>
+		<div class="disclaimer">* This specifies the date the filing was received by LTO, but this doesn't indicate whether the filing was returned (e.g. due to incomplete requirements). This also does not indicate whether or not the processing of the registration has finished, only when LTO has first received the filing.</div>
+	</div>
 
 </script>
