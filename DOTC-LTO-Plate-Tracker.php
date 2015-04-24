c2475527c8ea42124484298fb07632187f83fcf4
Herman <oherman@gmail.com>
2015-04-24 14:57:14 +0800

    Initial development

---
+++ DOTC-LTO-Plate-Tracker.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Plugin Name: DOTC-LTO Plate Tracker
+* Plugin URI: https://www.dotc.gov.ph
+* Description: Provides means of checking legitimacy of a given vehicle information, such as plate number.
+* Version: 1.0
+* Author: Gambit Technologies, Inc.
+* Author URI: http://gambit.ph
+* License: Proprietary
+* Text Domain: dotc-lto-pt
+* Domain Path: /languages
+*/
+	
+// Shortcode for checker form
+function checker_form() {
+	echo "I CHECK YOUR PLATES!";
+}
+
+// Shortcode for checker output?
+function checker_output() {
+	echo "";
+}
