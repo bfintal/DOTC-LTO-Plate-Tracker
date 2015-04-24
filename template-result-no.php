5192486892d1edfc4c4cfc58ace0d3a04d2757ec
Benjamin Intal <bfintal@gmail.com>
2015-04-24 20:33:50 +0800

    Updated copy

--- template-result-no.php
+++ template-result-no.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 <script type="text/html" id="tmpl-result-no">
 	
 	<div class="result-no">
-		<div>Not yet filed in LTO</div>
+		<div>Identification number not found, or the registration has not yet been received by LTO</div>
 		<div class="disclaimer">* For new filings, it may take up to 3-4 days for your filing to reflect in our database.</div>
 	</div>
 
