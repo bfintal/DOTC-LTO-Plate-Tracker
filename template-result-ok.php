b4bb1ee8e42265671b7a03a10c33de830ab0d6f2
Benjamin Intal <bfintal@gmail.com>
2015-04-30 16:37:51 +0800

    Corrected vehicle unit display

--- template-result-ok.php
+++ template-result-ok.php
@@ -6,9 +6,9 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	
 	<div class="result-ok">
 		<div class="date">Registration filing received on {{ data.date }}</div>
-		<% if ( typeof( data.unit ) !== "undefined" || data.unit !== "" ) { %>
+		<# if ( typeof( data.unit ) !== "undefined" && data.unit !== "" ) { #>
 			<div class="unit">Vehicle: {{ data.unit }}</div>
-		<% } %>
+		<# } #>
 		<div class="disclaimer">* This specifies the date the filing was received by LTO, but this doesn't indicate whether the filing was returned (e.g. due to incomplete requirements). This also does not indicate whether or not the processing of the registration has finished, only when LTO has first received the filing.</div>
 	</div>
 
